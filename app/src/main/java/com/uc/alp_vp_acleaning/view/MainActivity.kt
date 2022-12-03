3559241306e42e91c859f5f3354879dc34ae1e36
Davina T W <davinawijaya03@gmail.com>
2022-12-03 15:13:25 +0700

    nav error

--- app/src/main/java/com/uc/alp_vp_acleaning/view/MainActivity.kt
+++ app/src/main/java/com/uc/alp_vp_acleaning/view/MainActivity.kt
@@ -2,12 +2,51 @@ package com.uc.alp_vp_acleaning.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.uc.alp_vp_acleaning.R
+import com.uc.alp_vp_acleaning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewBind: ActivityMainBinding
+    lateinit var toggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBind.root)
+        viewBind.apply {
+            toggle = ActionBarDrawerToggle(this@MainActivity,drawerLayout,R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
 
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            navView.setNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.nav_technician_home->{
+                        Toast.makeText(this@MainActivity, "ini home", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.nav_technician_profile->{
+                        Toast.makeText(this@MainActivity, "ini profile", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.nav_technician_delete->{
+                        Toast.makeText(this@MainActivity, "ini delete", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.nav_technician_logout->{
+                        Toast.makeText(this@MainActivity, "ini home", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                true
+            }
+        }
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
